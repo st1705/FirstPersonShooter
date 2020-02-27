bf2a2c23227f02160f4db1932a5bd46889a53159
st1705 <39689688+st1705@users.noreply.github.com>
2020-02-27 13:22:50 +0100

    Added gun stuff
    -muzzleflash
    -sounds
    -target

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class GunScript : MonoBehaviour
 {
+    [Header("Gun Stats")]
+
     public float damage = 10f;
     public float range = 100f;
 
+    [Space]
+
+    [Header("Unity Setup")]
+
     public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject hitEffect;
+    public AudioSource gun;
+
+    [Space]
+
+    [Header("Audio")]
+
+    public AudioClip[] shots;
+    public AudioClip reload;
 
     private void Update()
     {
+        //Shooting button
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -17,10 +35,37 @@ public class GunScript : MonoBehaviour
 
     public void Shoot()
     {
+        //Visuals
+        muzzleFlash.Play();
+
+        //Get random shot clip and set to audiosource
+        gun.clip = shots[Random.Range(0, shots.Length)];
+        gun.Play();
+
+        //Shooting raycast
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            //Check if shooted object is a target
+            TargetScript target = hit.transform.GetComponent<TargetScript>();
+            if (target != null)
+            {
+                //Give damage to target
+                target.TakeDamage(damage);
+            }
+
+            //Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+            //if(rb != null)
+            //{
+            //    //give force to object
+            //    rb.AddForce();
+            //}
+
+
+            //make impact
+            Destroy(Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal)), 5f);
         }
     }
 }
---
+++ Assets/TargetScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    public float health = 50;
+
+    public void TakeDamage(float amount)
+    {
+        //Check if dead
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //Destroy obeject and spawn particles and play sound
+        Destroy(gameObject);
+    }
+}
