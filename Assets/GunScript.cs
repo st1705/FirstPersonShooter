5b06bac95e42bfec8c78f211519e7d51cb53650e
st1705 <39689688+st1705@users.noreply.github.com>
2020-02-27 12:45:44 +0100

    Added Gun Script
    Using raycast to get hit at range

---
+++ Assets/GunScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
